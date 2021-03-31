69b915dbbf7a1b9aa845412374a9f56a011c18d8
Alejandro Ramírez Gómez <alrg1720@gmail.com>
2021-03-30 20:19:12 -0600

    Seccion de URSL y cards terminado
    La seccion de generacion de urls esta completado, se recibe respuestas y
    se muestra en el DOM la información
    
    Pendiente: Validar URLS que sean válidas y optimizar código

--- src/js/index.js
+++ src/js/index.js
@@ -7,10 +7,9 @@ const inputLink = document.getElementById('frmLink')
 const api = 'https://api.shrtco.de/v2/shorten?url=';
 let   links = document.querySelector('#linksItems');
 let   mediaQuery = window.matchMedia("(min-width:960px)");
-mainToggle.addEventListener('click', () => {
-    mainHeader.classList.toggle('display-block')
-    document.body.classList.toggle('overflow-hidden')
-});
+
+
+
 
 mediaQuery.addEventListener('change', e => {
     console.log(e)
@@ -19,19 +18,33 @@ mediaQuery.addEventListener('change', e => {
     }
 })
 
+mainToggle.addEventListener('click', () => {
+    mainHeader.classList.toggle('display-block')
+    document.body.classList.toggle('overflow-hidden')
+});
+
+
 linksForm.addEventListener('submit', e =>{
     e.preventDefault();
     getLink(inputLink.value);
-    
 })
 
 links.addEventListener('click', e=>{
     console.log(e.target);
+    const button = e.target.classList.contains('items-button')
     if(e.target.classList.contains('items-button')){
         const selectText = e.target.parentElement.childNodes[0].textContent
         console.log(e.target.parentElement.childNodes[0].textContent);
         navigator.clipboard.writeText(selectText).then(function() {
-            console.log('Async: Copying to clipboard was successful!');
+            // console.log('Async: Copying to clipboard was successful!');
+            // console.log(button);
+            e.target.textContent= 'Copied';
+            e.target.style.background =  getComputedStyle(document.documentElement).getPropertyValue('--dark-violet')
+            linksForm.reset()
+            setTimeout(() => {
+                e.target.textContent = 'Copy'
+                e.target.style.background = getComputedStyle(document.documentElement).getPropertyPriority('--cyan')
+            }, 3000);
           }, function(err) {
             console.error('Async: Could not copy text: ', err);
           });
@@ -43,12 +56,17 @@ links.addEventListener('click', e=>{
 const getLink = (value) =>{
     let url = value
     let dataLink;
+    document.querySelector('.sk-cube-grid').style.display = 'block';
+    document.querySelector('.btnSubmit').style.display = 'none'
     console.log(`${api}${url}`);
     fetch(`${api}${url}`)
     .then ((rest) => rest.json())
     .then ((data) => {
         dataLink = data; 
         printLink(dataLink)  
+        document.querySelector('.sk-cube-grid').style.display = 'none';
+        document.querySelector('.btnSubmit').style.display = 'block'
+
     })
     .catch(error => console.log(error))    
 }
